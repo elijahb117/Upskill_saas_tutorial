9f6dae5ac0a86aea94dff1130f6cc3e825edfa28
Elijah Bunkley <eli98711@gmail.com>
2021-01-12 19:44:14 +0000

    Fixed whtaever i did wrong from video 123

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,11 @@
 class ContactMailer < ActionMailer::Base
     default to: 'eli98711@gmail.com'
    
-    def contact_email (name, email, body)
+    def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
-       
-        mail(from: email, subject: 'Contact form Message')
-    end 
+        mail(from: email, subject: 'Contact Form Message')
+    end
     
 end
